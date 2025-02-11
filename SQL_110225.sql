7a8eed5eeb18a75b5255ad7f453ef7c4bcbbd91b
Ramesh Kumar Prajapati <136189864+alwaysramesh@users.noreply.github.com>
2025-02-11 23:05:44 +0530

    Add files via upload

---
+++ SQL_110225.sql
@@ -0,0 +1,9 @@
+use cybrom;
+select*from emp;
+insert into emp values(8,'Rahul',22,'Bhopal',9000);
+-- CTE
+-- with customer_age as(select*from customer_salary where age=23);
+-- seelct id , name, age from customer_age;
+-- with
+-- union all
+-- RLIKE
